f173cf3cabc7b18fd66a8e0be00f0c20f86a9cae
k17pine <47183551+k17pine@users.noreply.github.com>
2019-04-04 03:21:46 +0300

    Update tests.py
    ticket:1,task:3

--- apps/homepage/tests.py
+++ apps/homepage/tests.py
@@ -13,3 +13,11 @@ class HomePageTests(SimpleTestCase):
         "put docstrings in your tests"
         response = self.client.get('/')
         self.assertEquals(response.status_code, 200)
+
+    def test_home_page_contains_correct_html(self):
+        response = self.client.get('/')
+        self.assertContains(response, '<h3>42 Coffee Cups Test Assignment</h')
+
+    def test_home_page_does_not_contain_incorrect_html(self):
+        response = self.client.get('/')
+        self.assertNotContains(response, 'Hi! I should not be on the page.')
